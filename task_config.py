f907b746dce01c9bac25a6b3fd52ae7480a0160f
Sampo Pyysalo <sampo.pyysalo@utu.fi>
2020-02-21 18:36:18 +0200

    Import

---
+++ task_config.py
@@ -0,0 +1,25 @@
+yle_labels = [
+    "Koulutus_ja_kasvatus",
+    "kulttuuri",
+    "Liikenne_ja_kuljetus",
+    "luonto",
+    "onnettomuudet",
+    "politiikka",
+    "rikokset",
+    "talous",
+    "terveys",
+    "urheilu",
+]
+
+ylilauta_labels = [
+    "ajoneuvot",
+    "hikky",
+    "kuntosali",
+    "muoti",
+    "pelit",
+    "penkkiurheilu",
+    "politiikka",
+    "seksuaalisuus",
+    "sota",
+    "televisio",
+]
